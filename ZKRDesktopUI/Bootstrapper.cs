df90f53c5d79fe542534bd251a77566b93b2b333
Zokir Sobirjonov <zokir5857@gmail.com>
2020-07-31 13:36:46 +0500

    Wire Up Login Form to API
    Connect the login form button to the authentication API endpoint (/token). Gets back the bearer token or an exception if failed.

--- ZKRDesktopUI/Bootstrapper.cs
+++ ZKRDesktopUI/Bootstrapper.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using Caliburn.Micro;
+using ZKRDesktopUI.Helpers;
 using ZKRDesktopUI.ViewModels;
 
 namespace ZKRDesktopUI
@@ -16,6 +18,11 @@ namespace ZKRDesktopUI
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
 
         protected override void Configure()
@@ -24,7 +31,8 @@ namespace ZKRDesktopUI
 
             _container
                 .Singleton<IWindowManager, WindowManager>()
-                .Singleton<IEventAggregator, EventAggregator>();
+                .Singleton<IEventAggregator, EventAggregator>()
+                .Singleton<IAPIHelper, APIHelper>();
 
             GetType().Assembly.GetTypes()
                 .Where(type => type.IsClass)
---
+++ ZKRDesktopUI/Helpers/IAPIHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ZKRDesktopUI.Models;
+
+namespace ZKRDesktopUI.Helpers
+{
+    public interface IAPIHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string username, string password);
+    }
+}
